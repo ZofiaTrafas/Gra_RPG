318785fec1e7509a87b0cdd905671b02dd069795
ZofiaTrafas <trafaszofia@gmail.com>
2020-06-04 18:44:49 +0200

    Dodaję drugi semestr

---
+++ drugi-semestr.py
@@ -0,0 +1,34 @@
+print("UWAGA! Uczelnia została zaatakowana przez wirusa Conid-20!")
+print("Od dzisiaj już nic nie będzie takie samo...")
+print("Uwaga: Twórcy gry nie biorą na siebie odpowiedzialności za ewentualne szkody psychiczne.")
+
+#WYBOR 1 - PROWADZĄCY
+print("Którego prowadzącego z programowania wybierasz? Francuz Dżokej Instruktor")
+prowadzacy = int(input())
+if prowadzacy == 1:
+    print("Twoim prowadzacym zostaje Francuz!")
+elif prowadzacy == 2:
+    print("Twoim prowadzacym zostaje Dzokej Instruktor!")
+#To jest "fałyszywy wybór, aby nadać poczucie normalnosci,
+#ale sie nie liczy, wiec guziki to moga byc falszywki"
+
+#Tutaj obrazek z pociągiem.
+#Mi by bardziej w koncepcji pasowało dać tylko napis "którą opcję wbierasz?
+# i klasyczny obrazek z tego dylematu, tylko że dodać na nim np. a i b
+#w odpowiednich miejscach"
+#i fanie by bylo dać ten napis którą opcję wybierasz jakimś glitchowym fontem
+print("Którą opcję wybierasz?")
+wybor = int(input())
+if wybor == 1:
+    print("Wybieraz pociagniecie dźwigni i zabicie 1 osoby")
+elif wybor == 2:
+    print("Nie ciągniesz za dźwignię i pociąg przejeżdża 5 osób")
+#GUZIKI A I B
+
+
+#tutaj też mroczny font by się przydał
+print("Teraz wszystko jest w twoich rękach")
+
+print("Nie ma odwagi bez dylematu lub charakteru, który nie jest ukształtowany przez wybory bardziej niż przez zwycięstwa.\n-Muriel Barbery-")
+print("Twórcy:\nJoanna Depta\nAlicja Raczunas\nZosia Trafas\nWeronika Wojtas")
+print("Muzyka:")
